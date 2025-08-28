9beec34e1615e13e68e53670c58bea12e4bfdb64
Alisher <alisheryondoshaliyev77@gmail.com>
2025-08-29 00:29:02 +0500

    task3 bajarildi

---
+++ task1.js
@@ -0,0 +1,13 @@
+/*1.
+To-Do List Loyihasi Tavsifi
+Obyekt yondashuvi: Biz to-do ro'yxatini boshqarish uchun obyekt yaratamiz. 
+Bu obyekt ro'yxat elementlarini saqlash, yangi vazifa qo'shish, vazifa o'chirish va ro'yxatni konsol 
+orqali ko'rish kabi metodlarga ega bo'lsin.
+Metodlar:
+addTask(task): Yangi vazifani ro'yxatga qo'shadi.
+removeTask(id): Berilgan indeksdagi vazifani o'chiradi.
+printTasks(): Barcha vazifalarni konsolga chiqaradi */
+
+let obj = {
+
+}
---
+++ task3.js
@@ -0,0 +1,30 @@
+/*Harflar va Raqamlarni Hisoblash
+Biror matn qabul qilib, unda nechta harf va raqam borligini hisoblaydigan funksiya yozing. 
+Natijani obyekt sifatida qaytaring.
+
+Misol uchun:
+
+countAll("Hello World") ➞ { "HARFLAR":  10, "RAQAMLAR": 0 }
+countAll("H3ll0 Wor1d") ➞ { "HARFLAR":  7, "RAQAMLAR": 3 }
+countAll("149990") ➞ { "HARFLAR": 0, "RAQAMLAR": 6 } */
+
+
+
+function countAll(str){
+    // const raqam = /[0-9]/;
+    // const harf = /[a-zA-Z]/;
+    let countNum = 0;
+    let countLetter = 0;
+    for (let char of str) {
+        if (/[0-9]/.test(char)) {
+            countNum++;
+        } else if (/[a-zA-Z]/.test(char)) {
+            countLetter++;
+        }
+    }
+    
+    return {HARFLAR: countLetter, RAQAMLAR: countNum};
+}
+
+console.log(countAll("Hello World"));
+
